a156a6c868b5f9c8b8ff187f7250e7753b8fe3b7
sihyun98 <sihyun98@cau.ac.kr>
2021-01-19 11:34:20 +0900

    change

---
+++ team3/back_server/src/main/java/com/hackathon3/back_server/web/HelloController.java
@@ -0,0 +1,17 @@
+package com.hackathon3.back_server.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/test")
+    public TestResponseDto test() {
+        return TestResponseDto.builder()
+                .code("OK")
+                .message("테스트성공!")
+                .build();
+    }
+
+}
