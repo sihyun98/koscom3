f5f0c6f5735e6f2942050b2fc0e9f2e0e06462a7
sihyun98 <sihyun98@cau.ac.kr>
2021-01-26 14:06:04 +0900

    member api

---
+++ team3_sts/back_server/src/main/java/com/hackathon3/back_server/dto/member/MemberUpdateRequestDto.java
@@ -0,0 +1,23 @@
+package com.hackathon3.back_server.dto.member;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+public class MemberUpdateRequestDto {
+
+	private Long id;
+	private String name;
+//	private String username;
+	private String password;
+	private String grade;
+	private int age;
+	private String job;
+	private String investOpt;
+	private int salary;
+	private int property;
+	private String profile;
+	
+}
